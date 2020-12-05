5d2c0a61a4627b2b313a69d2da96d68e159114e9
UditSinghParihar <uditsinghparihar96@gmail.com>
2020-12-05 23:57:32 +0530

    Commiting after aligning camera with anti parallel to normal.

--- getTopViewNormal.py
+++ getTopViewNormal.py
@@ -8,6 +8,13 @@ import cv2
 import matplotlib.pyplot as plt
 
 
+def display(pcd, T=np.identity(4)):
+	axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
+	axis.transform(T)
+
+	o3d.visualization.draw_geometries([pcd, axis])
+
+
 def getPointCloud(rgbFile, depthFile):
 	thresh = 5.6
 
@@ -27,11 +34,6 @@ def getPointCloud(rgbFile, depthFile):
 			X = (u - centerX) * Z / focalLength
 			Y = (v - centerY) * Z / focalLength
 			
-			# Xtemp = X; Ytemp = Y; Ztemp = Z
-			# X = Ztemp; Y = -Xtemp; Z = -Ytemp
-
-			# if(Z < 0): continue
-			
 			points.append((X, Y, Z))
 			colors.append(rgb.getpixel((u, v)))
 
@@ -43,50 +45,28 @@ def getPointCloud(rgbFile, depthFile):
 	pcd.colors = o3d.utility.Vector3dVector(colors/255)
 
 	downpcd = pcd.voxel_down_sample(voxel_size=0.03)
-	# points = np.asarray(downpcd.points)
-	
-	# ones = np.ones((points.shape[0], 1))
-	# points = np.hstack((points, ones))
-
-
-	# points = T @ points.T
-
-	# downpcd.points = o3d.utility.Vector3dVector((points.T)[:, 0:3])
-
-	# axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
-	# o3d.visualization.draw_geometries([downpcd, axis])
 	
 	return downpcd
 
 
-def rotation_matrix_from_vectors(vec1, vec2):
-	""" Find the rotation matrix that aligns vec1 to vec2
-	:param vec1: A 3d "source" vector
-	:param vec2: A 3d "destination" vector
-	:return mat: A transform matrix (3x3) which when applied to vec1, aligns it with vec2.
-	"""
+def rotationMatrixFromVectors(vec1, vec2):
 	a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
 	v = np.cross(a, b)
 	c = np.dot(a, b)
 	s = np.linalg.norm(v)
 	kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-	rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
-	return rotation_matrix
+	rotationMatrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+	return rotationMatrix
 
 
 def getNormals(pcd):
 	pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
 	pcd.orient_normals_towards_camera_location()
 
-	axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
-	o3d.visualization.draw_geometries([pcd, axis])
-	# o3d.visualization.draw_geometries([pcd])
-
-	# print(type(pcd.normals))
 	normals = np.asarray(pcd.normals)
-	# print(normals.shape)
-	# print(normals[:10, :])
-	print(np.mean(normals, axis=0))
+	surfaceNormal = np.mean(normals, axis=0)
+	
+	return surfaceNormal
 
 
 if __name__ == '__main__':
@@ -100,4 +80,12 @@ if __name__ == '__main__':
 
 	pcd = getPointCloud(rgbFile, depthFile)
 
-	getNormals(pcd)
+	surfaceNormal = getNormals(pcd)
+
+	zAxis = np.array([0, 0, 1])
+	rotationMatrix = rotationMatrixFromVectors(zAxis, -surfaceNormal)
+	T = np.identity(4)
+	T[0:3, 0:3] = rotationMatrix
+
+	display(pcd)
+	display(pcd, T)
